245a5e4e880efc9daab74822db2f07113b31a203
Slava Panfilenko <slava.panfilenko@outlook.com>
2025-02-16 20:00:24 +0400

    Add Games/GCD.php

---
+++ src/Games/GCD.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BrainGames\GCD;
+
+use function BrainGames\Interaction\showMessage;
+use function BrainGames\Runner\gameRun;
+
+function calculateGCD(int $a, int $b): int
+{
+    while ($b !== 0) {
+        $temp = $a % $b;
+        $a = $b;
+        $b = $temp;
+    }
+
+    return $a;
+}
+
+function generatePair(int $a, int $b): array
+{
+    $gcd = calculateGCD($a, $b);
+
+    return ["{$a} {$b}", $gcd];
+}
+
+function gcdGame(): void
+{
+    $description = "Find the greatest common divisor of given numbers.";
+    $question = fn() => generatePair(rand(1, 100), rand(1, 100));
+
+    gameRun($description, $question);
+}
