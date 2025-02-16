cca7db07d0382534654670c5c1096f388cf0d371
Slava Panfilenko <slava.panfilenko@outlook.com>
2025-02-16 18:28:14 +0400

    Update Games/Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -7,21 +7,17 @@ use function BrainGames\Runner\gameRun;
 
 function isEven(int $num): bool
 {
-    if ($num % 2 === 0) {
-        return true;
-    }
-
-    return false;
+    return $num % 2 === 0;
 }
 
-function evenGame()
+function generateNumber(int $num): array
+{
+    $answer = isEven($num) ? "Yes" : "No";
+    return [$num, $answer];
+}
+
+function evenGame(): void
 {
     $description = "Answer 'Yes' if the number is even, otherwise answer 'No'.";
-
-    gameRun($description, function() {
-        $num = rand(1, 99);
-        $answer = (isEven($num)) ? "Yes" : "No";
-    
-        return [$num, $answer];
-    });
+    gameRun($description, fn() => generateNumber(rand(1, 100)));
 }
