d54e086cbdcbcfed238e7781a99ac2fe7dd20649
Slava Panfilenko <slava.panfilenko@outlook.com>
2025-02-17 19:51:34 +0400

    Add Games/Progression.php

---
+++ src/Games/Progression.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BrainGames\Prog;
+
+use function BrainGames\Runner\gameRun;
+
+function hideElement(array $prog): array
+{
+    $index = rand(0, count($prog) - 1);
+    $answer = $prog[$index];
+    $prog[$index] = '..';
+
+    $question = implode(' ', $prog);
+
+    return [$question, $answer];
+}
+
+function generateProg(int $step, int $length): array
+{
+    $start = rand(1, 100);
+    $prog = [];
+
+    for ($i = 1; $i < $length; $i++) {
+        $prog[] = $start + $i * $step;
+    }
+
+    return hideElement($prog);
+}
+
+function progGame(): void
+{
+    $description = "What number is missing in the progression?";
+    $question = fn() => generateProg(rand(1, 11), 10);
+
+    gameRun($description, $question);
+}
