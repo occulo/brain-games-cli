60266d3cb414ff56c230da917e93fc82eaf364e1
Slava Panfilenko <slava.panfilenko@outlook.com>
2025-02-18 00:09:48 +0400

    Add Games/Prime.php

---
+++ src/Games/Prime.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BrainGames\Prime;
+
+use function BrainGames\Runner\gameRun;
+
+function isPrime(int $num): bool
+{
+    if ($num <= 1) {
+        return false;
+    }
+
+    for ($i = 2; $i <= sqrt($num); $i++) { 
+        if ($num % $i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function generateNumber(int $num): array
+{
+    $answer = isPrime($num) ? "Yes" : "No";
+
+    return [$num, $answer];
+}
+
+function primeGame(): void
+{
+    $description = "Answer 'Yes' if the number is prime, otherwise answer 'No'.";
+    $question = fn() => generateNumber(rand(1, 100));
+
+    gameRun($description, $question);
+}
