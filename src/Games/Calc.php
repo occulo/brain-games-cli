cfa9d82832654332f013212f6f696ad590fb8d1e
Slava Panfilenko <slava.panfilenko@outlook.com>
2025-02-19 16:28:38 +0400

    Update Games/Calc.php

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -4,23 +4,28 @@ namespace BrainGames\Calc;
 
 use function BrainGames\Engine\runGame;
 
-function generateExpression(int $a, int $b): array
+function getOperator(): string
 {
-    $operators = [
-        fn($x, $y) => ["{$x} + {$y}", $x + $y],
-        fn($x, $y) => ["{$x} - {$y}", $x - $y],
-        fn($x, $y) => ["{$x} * {$y}", $x * $y]
-    ];
-
-    $index = rand(0, count($operators) - 1);
-
-    return $operators[$index]($a, $b);
+    $operators = ['+', '-', '*'];
+    return $operators[array_rand($operators)];
 }
 
-function calcGame(): void
+function generateQuestion(int $a, int $b): array
+{
+    $operator = getOperator();
+    $expression = match ($operator) {
+        '+' => $a + $b,
+        '-' => $a - $b,
+        '*' => $a * $b
+    };
+
+    return ["{$a} {$operator} {$b}", $expression];
+}
+
+function startGame(): void
 {
     $description = 'What is the result of the expression?';
-    $question = fn() => generateExpression(rand(1, 100), rand(1, 100));
+    $question = fn() => generateQuestion(rand(1, 100), rand(1, 100));
 
     runGame($description, $question);
 }
