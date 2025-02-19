90a05fe01599fb8bbc75d6a4093c063d66ec9949
Slava Panfilenko <slava.panfilenko@outlook.com>
2025-02-19 16:29:20 +0400

    Update Games/Progression.php

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -13,7 +13,7 @@ function hideElement(array $prog): array
     return [implode(' ', $prog), $answer];
 }
 
-function generateProg(int $step, int $length): array
+function generateQuestion(int $step, int $length): array
 {
     $start = rand(1, 100);
     $prog = [];
@@ -25,10 +25,10 @@ function generateProg(int $step, int $length): array
     return hideElement($prog);
 }
 
-function progGame(): void
+function startGame(): void
 {
     $description = 'What number is missing in the progression?';
-    $question = fn() => generateProg(rand(1, 11), 10);
+    $question = fn() => generateQuestion(rand(1, 11), 10);
 
     runGame($description, $question);
 }
