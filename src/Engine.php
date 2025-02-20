6ab568a63135867e9a11aff703e23bf8ec34898b
Slava Panfilenko <slava.panfilenko@outlook.com>
2025-02-20 14:06:12 +0400

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -6,26 +6,27 @@ use function BrainGames\Interaction\welcomeUser;
 use function cli\line;
 use function cli\prompt;
 
-function runGame(string $description, callable $currentRound): void
+function runGame(string $description, callable $getQuestionAndAnswer): void
 {
     $name = welcomeUser();
     line($description);
 
-    $gameRounds = 3;
+    $roundsCount = 3;
 
-    for ($i = 0; $i < $gameRounds; $i++) {
-        [$question, $correctAnswer] = $currentRound();
+    for ($i = 0; $i < $roundsCount; $i++) {
+        [$question, $correctAnswer] = $getQuestionAndAnswer();
 
         line("Question: {$question}");
         $userAnswer = prompt("Answer");
 
-        if (strcasecmp($correctAnswer, $userAnswer) == 0) {
-            line("Correct!");
-        } else {
+        if (strcasecmp($correctAnswer, $userAnswer) !== 0) {
             line("'{$userAnswer}' is the wrong answer. The correct answer was '{$correctAnswer}'.");
             line("Let's try again, {$name}!");
             return;
         }
+
+        line("Correct!");
+        continue;
     }
 
     line("Congratulations, {$name}!");
